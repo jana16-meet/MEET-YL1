8cecc8714228f82a02bf72a4ba6e1016c6152ae5
Jana Dajani <jana16@meet.mit.edu>
2015-01-15 20:07:31 +0200

    opp2

---
+++ oop2.py
@@ -0,0 +1,17 @@
+class animal :
+	def __init__(self,name,age,color,size) :
+		self.name=name
+		self.age=age
+		self.color=color
+		self.size=size
+
+
+
+
+a=("sadek",22,"yellow","tiny")
+print(a.name)
+print(a.age)
+print(a.color)
+print(a.size)
+
+
